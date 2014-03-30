3edf8109f6edda4bb658b1e9d7a04c7df2aaaaf4
Skabed <gmadorell@gmail.com>
2014-03-30 17:11:03 +0200

    Linear regression with vectorization.

--- programming_exercises/week 2 - Linear Regression/mlclass-ex1/gradientDescent.m
+++ programming_exercises/week 2 - Linear Regression/mlclass-ex1/gradientDescent.m
@@ -6,6 +6,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
+amount_theta = size(theta, 1);
 
 for iter = 1:num_iters
 
@@ -17,27 +18,31 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
     predictions = X * theta;
     
     
-%     delta = sum(predictions - y) * X;
-%     theta = theta - alpha * (1/m) * delta;
     
-    amount_theta = size(theta, 1);
+    % Different implementations (from less to more vectorized).
     
-    new_theta = zeros(amount_theta, 1);
-
-    for i=1:amount_theta
-        sum = 0;
-        for j=1:m
-            sum = sum + (predictions(j, 1) - y(j, 1)) * X(j, i);
-        end
-        
-        new_theta(i) = theta(i) - alpha * (1/m) * sum;
-    end
+%     % First implementation - No vectorization at all.
+%     for i=1:amount_theta
+%         sum = 0;
+%         for j=1:m
+%             sum = sum + (predictions(j, 1) - y(j, 1)) * X(j, i);
+%         end
+%         
+%         theta(i) = theta(i) - alpha * (1/m) * sum;
+%     end
     
-    theta = new_theta;
+%     % Second implementation - Inner loop vectorized.
+%     for i=1:amount_theta        
+%         sum = (predictions - y)' * X(:, i);       
+%         theta(i) = theta(i) - alpha * (1/m) * sum;
+%     end
+    
+    % Third implementation - Full vectorization.
+    delta = X' * (predictions - y);
+    theta = theta - alpha * (1/m) * delta;
 
     % ============================================================
 
