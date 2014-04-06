22d218487018a8ceda9a9cfc4ce49b237d7e8130
Skabed <gmadorell@gmail.com>
2014-04-06 23:25:54 +0200

    Added regularization theory and solved the first problems of the Linear Regression programming part.

---
+++ programming_exercises/week 3 - Logistic Regression/mlclass-ex2/sigmoid.m
@@ -0,0 +1,18 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+% Arrayfun applies the function to every single element of the matrix.
+g = arrayfun(@apply_sigmoid, z);
+
+    function sigmoid_value = apply_sigmoid(number)
+        sigmoid_value = 1 / (1 + exp(-number));
+    end
+
+end
