05f4c8757d1f4aae5fac2490319bf24ccf4d2d22
Skabed <gmadorell@gmail.com>
2014-04-20 17:18:45 +0200

    Finished neural networks training exercise

--- programming_exercises/week 5 - Neural Networks Learning/mlclass-ex4/nnCostFunction.m
+++ programming_exercises/week 5 - Neural Networks Learning/mlclass-ex4/nnCostFunction.m
@@ -131,7 +131,11 @@ end
 regularization = regularization * lambda / (2*m);
 J = J + regularization;
 
-
+% NN Gradient Regularization
+% Note that the first column of thetha should not be regularized as it's used
+% for the bias term.
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + ((lambda / m) * Theta1(:,2:end));
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + ((lambda / m) * Theta2(:,2:end));
 
 
 
